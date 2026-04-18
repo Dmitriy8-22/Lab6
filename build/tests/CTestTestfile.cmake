fd6bbf41c31ac96c9d716d43b86419de85119634
Dmitriy8-22 <dmitrijkiselev36@gmail.com>
2026-04-18 20:19:29 +0300

    Fix

--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/Lab5/banking/tests
-# Build directory: /home/vboxuser/Lab5/banking/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/vboxuser/Lab5/banking/build/tests/run_tests[1]_include.cmake")
